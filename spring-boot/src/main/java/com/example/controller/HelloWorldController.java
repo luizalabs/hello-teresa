5092db127cdf4b1cf509bd932c13eb5fa5f76384
eduardocortes <eduardo.lima@fcamara.com.br>
2017-03-22 15:56:12 -0300

    Hello world spring-boot!

---
+++ spring-boot/src/main/java/com/example/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping("/")
+	public String showMessage(){
+		return "hello world";
+	}
+}
